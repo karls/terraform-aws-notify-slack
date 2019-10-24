b11101921350508bb814dc62a97d583b80353402
Karl Sutt <karl@sutt.ee>
2019-10-24 14:37:13 +0300

    Syntax update

--- main.tf
+++ main.tf
@@ -35,13 +35,13 @@ resource "aws_lambda_permission" "sns_notify_slack_fallback" {
 }
 
 data "null_data_source" "lambda_file" {
-  inputs {
+  inputs = {
     filename = "${substr("${path.module}/functions/notify_slack.py", length(path.cwd) + 1, -1)}"
   }
 }
 
 data "null_data_source" "lambda_archive" {
-  inputs {
+  inputs = {
     filename = "${substr("${path.module}/functions/notify_slack.zip", length(path.cwd) + 1, -1)}"
   }
 }
